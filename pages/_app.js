ebc4ee5ee5762e3961fda244b63f2f140431a8bb
DanielSledz03 <daniel.sledz@pluscode.io>
2023-10-30 22:49:44 +0100

    upgrade npm modules verion

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/index.css'
-import Footer from '../components/Footer/Footer'
-import Nav from '../components/Nav/Nav'
-import { AnimatePresence } from 'framer-motion'
-import Head from 'next/head'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/index.css";
+import Footer from "../components/Footer/Footer";
+import Nav from "../components/Nav/Nav";
+import { AnimatePresence } from "framer-motion";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,13 +13,17 @@ function MyApp({ Component, pageProps }) {
         <title>Gabinet kosmetologiczny</title>
       </Head>
       <Nav />
-      <AnimatePresence exitBeforeEnter initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
+      <AnimatePresence
+        mode="wait"
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
         <Component {...pageProps} />
       </AnimatePresence>
 
       <Footer />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
